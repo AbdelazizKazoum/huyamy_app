c70077297dd1eba8ff608f00761077c999f15e03
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-10-18 14:37:25 +0100

    feat: Remove Head component and add metadata for manifest in layout

--- src/app/[locale]/(client)/layout.tsx
+++ src/app/[locale]/(client)/layout.tsx
@@ -1,9 +1,12 @@
-import Head from "next/head";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import NoticeBar from "@/components/layout/NoticeBar";
 import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 import ToasterProvider from "@/providers/ToasterProvider";
+
+export const metadata = {
+  manifest: "/manifest-client.json",
+};
 
 export default function RootLayout({
   children,
@@ -12,9 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Head>
-        <link rel="manifest" href="/manifest-client.json" />
-      </Head>
       <ToasterProvider />
       <ServiceWorkerRegister />
       <NoticeBar />
