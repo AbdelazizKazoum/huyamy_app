da46d02dd2ee71b12e4564a32430cf3521b36a93
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-10-18 14:05:42 +0100

    feat: Update layout to use client manifest and remove unused metadata

--- src/app/[locale]/(client)/layout.tsx
+++ src/app/[locale]/(client)/layout.tsx
@@ -1,12 +1,9 @@
+import Head from "next/head";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import NoticeBar from "@/components/layout/NoticeBar";
 import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 import ToasterProvider from "@/providers/ToasterProvider";
-
-export const metadata = {
-  manifest: "/manifest-admin.json",
-};
 
 export default function RootLayout({
   children,
@@ -15,11 +12,14 @@ export default function RootLayout({
 }>) {
   return (
     <>
+      <Head>
+        <link rel="manifest" href="/manifest-client.json" />
+      </Head>
       <ToasterProvider />
       <ServiceWorkerRegister />
       <NoticeBar />
       <Header />
-      <main className={``}>{children}</main>
+      <main>{children}</main>
       <Footer />
     </>
   );
