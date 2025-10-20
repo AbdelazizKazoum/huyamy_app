ee22719645b0c22c9ad9e2b99f145f96bf109fec
Abdelaziz kazoum <abdukazoum@gmail.com>
2025-10-21 00:28:01 +0200

    Refactor code structure for improved readability and maintainability

--- src/config/site.ts
+++ src/config/site.ts
@@ -74,8 +74,8 @@ export const siteConfig: SiteConfig = {
   },
   url: "https://huyamybio.com",
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://huyamybio.com",
-  logo: "/images/huyami_logo.jpeg",
-  ogImage: "/images/huyami_logo.jpeg", // Default OG image
+  logo: "/images/logo2.png",
+  ogImage: "/images/logo2.png", // Default OG image
   keywords: {
     ar: [
       "زيت الأرغان",
